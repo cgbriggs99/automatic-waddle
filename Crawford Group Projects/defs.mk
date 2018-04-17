eebb1028cef95df99228161c066ba1a80f105db1
cgbriggs99 <cgbriggs99@gmail.com>
2018-04-17 17:03:17 -0400

    Added makegen.py to make paths.mk

--- Crawford Group Projects/defs.mk
+++ Crawford Group Projects/defs.mk
@@ -1,3 +1,5 @@
+include paths.mk
+
 OPTIMIZE=-O3 -Ofast -faggressive-loop-optimizations -falign-functions -fauto-inc-dec -fbranch-count-reg\
 -fbranch-probabilities -fbranch-target-load-optimize -fdce -fdelete-dead-exceptions -fdse -fexpensive-optimizations\
 -ffinite-math-only -fforward-propagate -fif-conversion -fipa-cp
@@ -5,5 +7,5 @@ OPTIMIZE=-O3 -Ofast -faggressive-loop-optimizations -falign-functions -fauto-inc
 DEBUG=-DDEBUG -g3 -ggdb
 EXCEL=$(DEBUG) -DEXCEL
 
-CFLAGS=-I C:\lapack\include $(DEBUG)
+CFLAGS+=$(DEBUG)
 export OPTIMIZE, DEBUG, EXCEL, CFLAGS
---
+++ Crawford Group Projects/makegen.py
@@ -0,0 +1,69 @@
+#!/bin/python3
+
+import os
+import sys
+
+# Default directories and names
+__cblas_h = "cblas.h"
+__lapacke_h = "lapacke.h"
+__cblas_lib = "cblas"
+__lapacke_lib = "lapacke"
+__blas_lib = "blas"
+__lapack_lib = "lapack"
+
+#Windows
+
+
+def __setup_make(fd : int):
+    print("# This file was generated automatically.", file = fd)
+    
+def __write_libs(fd, blas_path, lapack_path, cblas_path, lapacke_path, blas_name, lapack_name, cblas_name, lapacke_name):
+    print("BLAS_LIB=%s"%(blas_path), file = fd)
+    print("LAPACK_LIB=%s"%(lapack_path), file = fd)
+    print("BLAS_NAME=%s"%(blas_name), file = fd)
+    print("LAPACK_NAME=%s"%(lapack_name), file = fd)
+    print("CBLAS_LIB=%s"%(cblas_path), file = fd)
+    print("LAPACKE_LIB=%s"%(lapacke_path), file = fd)
+    print("CBLAS_NAME=%s"%(cblas_name), file = fd)
+    print("LAPACKE_NAME=%s"%(lapacke_name), file = fd)
+    
+def __write_includes(fd, blas_name, lapack_name):
+    print("CBLAS_HEADER=%s"%(blas_name), file = fd)
+    print("LAPACKE_HEADER=%s"%(lapack_name), file = fd)
+
+def __write_export(fd):
+    print("export", file = fd)
+    
+def generate():
+    paths = open("defs.mk", "x")
+    blas_lib = input("BLAS library name (default 'blas'): ")
+    blas_lib_path = input("BLAS library location (default to check path): ")
+    lapack_lib = input("LAPACK library name (default 'lapack'): ");
+    lapack_lib_path = input("LAPACK library location (default to check path): ")
+    cblas_lib = input("CBLAS library name (default 'cblas'): ");
+    cblas_lib_path = input("CBLAS library location (default to check path): ")
+    cblas_header = input("CBLAS header file full name (default to 'cblas.h'): ")
+    lapacke_lib = input("LAPACKE library name (default 'lapacke'): ");
+    lapacke_lib_path = input("LAPACKE library location (default to check path): ")
+    lapacke_header = input("LAPACKE header file full name (default to 'lapacke.h'): ")
+    
+    if blas_lib == '':
+        blas_lib = __blas_lib
+    if lapack_lib == '':
+        lapack_lib = __lapack_lib
+    if cblas_lib == '':
+        cblas_lib = __cblas_lib
+    if lapacke_lib == '':
+        lapacke_lib = __lapacke_lib
+    if cblas_header == '':
+        cblas_header = __cblas_h
+    if lapacke_header == '':
+        lapacke_header = __lapacke_h
+    
+    __setup_make(paths)
+    __write_libs(paths, blas_path, lapack_path, cblas_path, lapacke_path, blas_lib, lapack_lib, cblas_lib, lapacke_lib)
+    __wirte_includes(paths, cblas_header, lapacke_header)
+    __write_export(paths)
+    close(paths)
+    
+generate()
