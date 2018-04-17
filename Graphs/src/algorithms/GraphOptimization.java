868b292e63387761885477d02b8fa051c1778fd6
cgbriggs99 <cgbriggs99@gmail.com>
2018-04-17 10:31:47 -0400

    Edit GraphOptimizations.java

--- Graphs/src/algorithms/GraphOptimization.java
+++ Graphs/src/algorithms/GraphOptimization.java
@@ -18,13 +18,22 @@ public class GraphOptimization {
 		}
 	}
 
+	private static final double coulombConst = 1, hookeConst = 1;
+	
 	private static Point force(Node<Double, Point> p1, Node<Double, Point> p2) {
 		Point force = new Point();
 		
 		if(p1.getNeighbors().contains(p2) || p2.getNeighbors().contains(p1)) {
-			force.x = p2.getData().x - p1.getData().x;
-			
+			force.x = (p2.getData().x - p1.getData().x) * hookeConst;
+			force.y = (p2.getData().y - p1.getData().y) * hookeConst;
+		} else {
+			force.x = 0;
+			force.y = 0;
 		}
+		double dist = Math.hypot(p2.getData().x - p1.getData().x, p2.getData().y - p1.getData().y);
+		
+		force.x -= coulombConst * (p2.getData().x - p1.getData().x) / (dist * dist * dist);
+		force.y -= coulombConst * (p2.getData().y - p1.getData().y) / (dist * dist * dist);
 	}
 
 }
