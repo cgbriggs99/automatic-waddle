03c565d75c2b8438d0804d74f824d05245c10ca1
connor <connor@connor-briggs-xubuntu-17.10>
2018-04-20 14:44:14 -0400

    Edit .gitignore to not ignore important files.

---
+++ Crawford Group Projects/Default Files/defs.mk
@@ -0,0 +1,16 @@
+
+#Current operating system.
+CURR_OS=
+
+#Suffix for an archive.
+ARCH_SUFF=
+
+#Suffix for an object file.
+OBJ_SUFF=
+
+#Suffix for an executable.
+EXEC_SUFF=
+
+#Removal command
+RM=
+export
---
+++ Crawford Group Projects/Default Files/paths.mk
@@ -0,0 +1,20 @@
+#This makefile is for the various paths and names specific to your computer.
+
+#Path to the BLAS and LAPACK libraries.
+LAPACK_LIB_PATH=
+BLAS_LIB_PATH=
+LAPACKE_LIB_PATH=
+CBLAS_LIB_PATH=
+
+#Library names
+LAPACK_LIB_NAME=
+BLAS_LIB_NAME=
+LAPACKE_LIB_NAME=
+CBLAS_LIB_NAME=
+
+#Header names and locations
+LAPACKE_HEADER_PATH=
+LAPACKE_HEADER_NAME=
+CBLAS_HEADER_PATH=
+CBLAS_HEADER_NAME=
+export
