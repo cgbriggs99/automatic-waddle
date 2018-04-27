4b1fb602dd2b662b81f2cebf4b1e4f510cfad671
cgbriggs99 <cgbriggs99@gmail.com>
2018-04-27 11:54:35 -0400

    Added headers for calculations

---
+++ Cpp Computational Chemistry/calculations.hpp
@@ -0,0 +1,15 @@
+/*
+ * calculations.hpp
+ *
+ *  Created on: Apr 27, 2018
+ *      Author: Connor
+ */
+
+#ifndef CALCULATIONS_HPP_
+#define CALCULATIONS_HPP_
+
+#include "calculations/calculations1.hpp"
+
+
+
+#endif /* CALCULATIONS_HPP_ */
